41cd302938c7e6a4f8b370fb52bf67401cb898eb
Andy Wendt <andy@awendt.com>
2020-12-07 20:15:21 -0700

    Add initial files

---
+++ lib/pdf_links_checker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "pdf_links_checker/version"
+
+module PdfLinksChecker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pdf_links_checker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PdfLinksChecker
+  VERSION = "0.1.0"
+end
