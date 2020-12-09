22a523a8a5be0a533b755ce21e4f8842f3f21678
Andy Wendt <andy@awendt.com>
2020-12-08 22:15:58 -0700

    Add in additional method to ensure it is a link

---
+++ lib/pdf_links_checker/pdf_document.rb
@@ -0,0 +1,19 @@
+module PdfLinksChecker
+  class PdfDocument
+    def initialize(pdf_doc)
+      @pdf_doc = pdf_doc
+    end
+
+    def links
+      pages.reduce([]) { |links, page| links.concat(page.links) }.then { |links| Links.new(links) }
+    end
+
+    private
+
+    def pages
+      pdf_doc.pages.map { |page| Page.new(page, Annotations.new(pdf_doc)) }
+    end
+
+    attr_reader :pdf_doc
+  end
+end
